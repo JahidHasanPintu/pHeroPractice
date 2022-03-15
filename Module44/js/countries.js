174b63753f64751e7ec54a7a4f880c57a1d9aab6
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-03-15 21:31:05 +0600

    flags added

--- Module44/js/countries.js
+++ Module44/js/countries.js
@@ -4,7 +4,22 @@ const loadCountries = () =>{
     .then(data =>displayCountries(data));
 }
 
-loadCountries();
+
 const displayCountries = countries =>{
-    console.log(countries);
+    // console.log(countries);
+    const allCountriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(allCountriesHTML[0]);
+    const container =  document.getElementById('countries');
+    container.innerHTML= allCountriesHTML.join(' ');
 }
+
+const getCountryHTML = country =>{
+    return `
+     <div> 
+        <h2>${country.name.common}</h2>
+        <img src="${country.flags.png}">
+     </div>
+    `
+}
+
+loadCountries();
