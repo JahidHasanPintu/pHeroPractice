24b1ce503a14576c7efd01dcce1441ee4e6418c8
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-03-16 09:44:37 +0600

    module 44/6 complete

--- Module44/js/countries.js
+++ Module44/js/countries.js
@@ -6,20 +6,47 @@ const loadCountries = () =>{
 
 
 const displayCountries = countries =>{
-    // console.log(countries);
+    console.log(countries);
     const allCountriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(allCountriesHTML[0]);
     const container =  document.getElementById('countries');
     container.innerHTML= allCountriesHTML.join(' ');
 }
+// const getCountryHTML = country =>{
+//     // Distructuring option : 1 
+//     const {name,flags}=country;
+//     return `
+//      <div class="country"> 
+//         <h2>${name.common}</h2>
+//         <img src="${flags.png}">
+//      </div>
+//     `
+// }
 
-const getCountryHTML = country =>{
+// option 2
+const getCountryHTML = ({name,flags,area,capital,languages,population}) =>{
+    
     return `
      <div class="country"> 
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.png}">
+        <h2>${name.common}</h2>
+        <p>Area: ${area} </p>
+        <p>Population : ${population} </p>
+        <p>Capital: ${capital} </p>
+        <p>Language: ${languages} </p>
+
+        <img src="${flags.png}">
      </div>
     `
 }
+
+// // This is the origninal one 
+// const getCountryHTML = country =>{
+//     return `
+//      <div class="country"> 
+//         <h2>${country.name.common}</h2>
+//         <img src="${country.flags.png}">
+//      </div>
+//     `
+// }
 
 loadCountries();
